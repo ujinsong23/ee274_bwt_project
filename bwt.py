9c3891a3d6d0e458e2da2a56258d1a70c35a7b0f
ujinsong23 <150768379+ujinsong23@users.noreply.github.com>
2023-12-09 22:35:22 -0800

    Update bwt.py
    - Only includes definition of necessary functions for compression/searching
    - and relevant testing for each module

--- bwt.py
+++ bwt.py
@@ -1,5 +1,4 @@
 from typing import Tuple
-import argparse
 import numpy as np
 import bitarray
 from scl.core.data_encoder_decoder import DataDecoder, DataEncoder
@@ -9,16 +8,6 @@ from scl.compressors.huffman_coder import HuffmanEncoder,HuffmanDecoder
 from scl.core.encoded_stream import EncodedBlockReader, EncodedBlockWriter
 from scl.core.prob_dist import ProbabilityDist
 from scl.utils.bitarray_utils import BitArray, uint_to_bitarray, bitarray_to_uint
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--decompress", help="Decompression mode if called.", action='store_true')
-parser.add_argument("-i", "--input", required=True, type=str, help="Input file directory")
-parser.add_argument("-o", "--output", type=str,
-                    help="Output file directory, locate same as input if undefined")
-parser.add_argument("--delimiter", default=130, type=int,
-                    help="Decimal ascii code for delimiter. It should never occurs in the input sequence. 130 if undefined")
-
 
 DELIMITER = chr(130)
 
@@ -747,64 +736,3 @@ def test_BWT_compression():
     decoded_result = bwt.inverse(decoded_bwt)
 
     assert decoded_result.data_list == data_block.data_list
-
-if __name__ == "__main__":
-
-    args = parser.parse_args()
-
-    BLOCKSIZE = 10_000_000 
-    DELIMITER = chr(args.delimiter)
-    bwt = BurrowsWheelerTransform()
-    mtf = MoveToFrontTransform()
-    rle = RunLengthEncoding()
-    
-    if args.decompress:
-
-        assert args.input.endswith(".bwtz"), "input format error : .bwtz file is required for BWT decompression"
-        inp = args.input
-
-        if args.output:
-            assert args.input.endswith(".txt"), "output format error : .txt file is required for BWT decompression"
-            out = args.output
-        else :
-            out = inp[:-5]+'_decompressed.txt'
-
-        decoder = HuffmanEmpiricalDecoder()
-
-
-        with EncodedBlockReader(inp) as reader:
-            with TextFileDataStream(out, "w") as fds:
-            
-                while True:
-
-                    data_block = reader.get_block()
-                    if data_block == None:
-                        break
-
-                    decoded_block = bwt.inverse(mtf.inverse(rle.inverse(decoder.decode_block(data_block)[0])))
-                    fds.write_block(decoded_block)
-                    
-    else:
-
-        assert args.input.endswith(".txt"), "input format error : .txt file is required for BWT compression"
-        inp = args.input
-        
-        if args.output:
-            assert args.input.endswith(".bwtz"), "output format error : .bwtz file is required for BWT decompression"
-            out = args.output
-        else :
-            out = inp[:-4]+'.bwtz'
-
-        encoder = HuffmanEmpiricalEncoder()
-
-        with EncodedBlockWriter(out) as writer:
-            with TextFileDataStream(inp, "r") as fds:
-                
-                while True:
-
-                    data_block = fds.get_block(block_size = BLOCKSIZE)
-                    if data_block == None:
-                        break   
-
-                    encoded_block = encoder.encode_block(rle.forward(mtf.forward(bwt.forward(data_block))))
-                    writer.write_block(encoded_block)
